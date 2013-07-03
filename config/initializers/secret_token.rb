425c2439c6ead84c041a32dbf42b7c27e5b756eb
Alfredo Pantoja <chololoy@gmail.com>
2013-07-03 12:53:34 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4DeviceCancan::Application.config.secret_key_base = '596246a087cde3641b86ceb0a4a93a5aaf6c016c20be903380974b05361d2c696a6e39d384bee88f808eae80eb29fcb404b40a3e67af30c411e7d13400a6c465'
